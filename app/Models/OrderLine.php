e039c799703c4a0692cb840df7b19e0f5e287750
Hariprsanth <mghprasanth98@gmail.com>
2026-09-14 01:21:32 +0530

    migration models and fack data method  code add

---
+++ app/Models/OrderLine.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderLine extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['order_id', 'product_id', 'quantity', 'unit_price', 'tax_percentage', 'line_subtotal', 'line_tax', 'line_total'];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
